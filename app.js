6fcb5fd5723b7e45f2c6e2886d76e180d9f95d38
Oliveck10 <oliveck10@gmail.com>
2016-10-14 18:29:27 +0800

    first version of router handling

---
+++ app.js
@@ -0,0 +1,55 @@
+/* eslint-disable new-cap */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+// user data (the json object which shoud be get from the backend api)
+const users = [
+  {
+    id: 1,
+    name: 'Joe',
+    age: 18,
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 22,
+  },
+];
+
+app.use('/', router);
+
+router.use('/', bodyParser.urlencoded({ extended: false }));
+router.use('/', bodyParser.json());
+
+// router.use('/', (req, res) => {
+//   res.send('Route use msg');
+// });
+
+router.get('/', (req, res) => {
+  res.send('<h1>首頁</h1>');
+});
+
+router.post('/', (req, res) => {
+  res.send('Route post msg');
+});
+
+router.post('/api/body', (req, res) => {
+  res.send(JSON.stringify(req.body));
+});
+
+router.use('/api/user/:id', (req, res) => {
+  res.json(users[req.params.id - 1]);
+});
+
+router.use('/api', (req, res) => {
+  res.json(req.query);
+});
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
